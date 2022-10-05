316881b520b520de24a98c2d2ac90781921e5604
ayazev <yazev73@gmail.com>
2022-10-05 19:35:56 +0300

    2. Статистика по коллекции. [#45889 #348928]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,45 @@
+package ru.job4j.question;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+
+        int added = 0;
+        int changed = 0;
+        int deleted = 0;
+        Map<Integer, User> previousMap = previous.stream().collect(Collectors.toMap(User::getId, e -> e));
+        Map<Integer, User> currentMap = current.stream().collect(Collectors.toMap(User::getId, e -> e));
+        for (Integer key : previousMap.keySet()) {
+            if (!currentMap.containsKey(key)) {
+                deleted++;
+            } else {
+                if (currentMap.get(key) != previousMap.get(key)) {
+                    changed++;
+                }
+            }
+        }
+        for (Integer key : currentMap.keySet()) {
+            if (!(previousMap.containsKey(key))) {
+                added++;
+            }
+        }
+
+        return new Info(added, changed, deleted);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        return super.equals(obj);
+    }
+}
---
+++ src/test/java/ru/job4j/question/AnalizeTest.java
@@ -0,0 +1,90 @@
+package ru.job4j.question;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class AnalizeTest {
+
+    @Test
+    void whenNotChanged() {
+        User u1 = new User(1, "A");
+        User u2 = new User(2, "B");
+        User u3 = new User(3, "C");
+        Set previous = Set.of(u1, u2, u3);
+        Set current = Set.of(u1, u2, u3);
+        assertThat(Analize.diff(previous, current)).isEqualTo(new Info(0, 0, 0));
+    }
+
+    @Test
+    void whenOneChanged() {
+        User u1 = new User(1, "A");
+        User u2 = new User(2, "B");
+        User u3 = new User(3, "C");
+        Set previous = Set.of(u1, u2, u3);
+        Set current = Set.of(u1, new User(2, "BB"), u3);
+        assertThat(Analize.diff(previous, current)).isEqualTo(new Info(0, 1, 0));
+    }
+
+    @Test
+    void whenOneDeleted() {
+        User u1 = new User(1, "A");
+        User u2 = new User(2, "B");
+        User u3 = new User(3, "C");
+        Set previous = Set.of(u1, u2, u3);
+        Set current = Set.of(u1, u3);
+        assertThat(Analize.diff(previous, current)).isEqualTo(new Info(0, 0, 1));
+    }
+
+    @Test
+    void whenOneAdded() {
+        User u1 = new User(1, "A");
+        User u2 = new User(2, "B");
+        User u3 = new User(3, "C");
+        Set previous = Set.of(u1, u2, u3);
+        Set current = Set.of(u1, u2, u3, new User(4, "D"));
+        assertThat(Analize.diff(previous, current)).isEqualTo(new Info(1, 0, 0));
+    }
+
+    @Test
+    void whenAllChanged() {
+        User u1 = new User(1, "A");
+        User u2 = new User(2, "B");
+        User u3 = new User(3, "C");
+        Set previous = Set.of(u1, u2, u3);
+        Set current = Set.of(new User(1, "AA"), u2, new User(4, "D"));
+        assertThat(Analize.diff(previous, current)).isEqualTo(new Info(1, 1, 1));
+    }
+
+    @Test
+    void whenOneAddedOneDeletedOneChanged() {
+        User u1 = new User(1, "A");
+        User u2 = new User(2, "B");
+        User u3 = new User(3, "C");
+        Set previous = Set.of(u1, u2);
+        Set current = Set.of(new User(1, "AA"), u3);
+        assertThat(Analize.diff(previous, current)).isEqualTo(new Info(1, 1, 1));
+    }
+
+    @Test
+    void whenAllDeleted() {
+        User u1 = new User(1, "A");
+        User u2 = new User(2, "B");
+        User u3 = new User(3, "C");
+        Set previous = Set.of(u1, u2, u3);
+        Set current = Set.of();
+        assertThat(Analize.diff(previous, current)).isEqualTo(new Info(0, 0, 3));
+    }
+
+    @Test
+    void whenEmptyAndAllAdded() {
+        User u1 = new User(1, "A");
+        User u2 = new User(2, "B");
+        User u3 = new User(3, "C");
+        Set previous = Set.of();
+        Set current = Set.of(u1, u2, u3);
+        assertThat(Analize.diff(previous, current)).isEqualTo(new Info(3, 0, 0));
+    }
+}
