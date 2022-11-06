66fde2e650f00289f344707cf4acecf7be302765
landiz <landizm@yandex.ru>
2022-11-06 18:14:56 +0300

    2. Анализ доступности сервера. [#859 #358340]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,48 +1,33 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class Analysis {
-
-    private static final Map<String, String> VALUES = new TreeMap<>();
-
     public static void unavailable(String source, String target) {
         boolean serverDown = false;
-        List<String> list = new ArrayList<>();
-        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-            read.lines().forEach(s -> list.add(String.valueOf(s)));
-            for (String line : list) {
-                String[] words = (line.split(" "));
-                VALUES.put(words[1], words[0]);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String logLine = null;
-        List<String> listOut = new ArrayList<>();
-        for (Map.Entry<String, String> entry : VALUES.entrySet()) {
-
-            if ("400".equals(entry.getValue()) || "500".equals(entry.getValue())) {
-                if (!serverDown) {
-                    logLine = entry.getKey() + ";";
-                    serverDown = true;
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+            line = reader.readLine();
+            try (FileOutputStream out = new FileOutputStream(target)) {
+                while (line != null) {
+                    String[] words = (line.split(" "));
+                    if ("400".equals(words[0]) || "500".equals(words[0])) {
+                        if (!serverDown) {
+                            out.write(words[1].getBytes());
+                            out.write(";".getBytes());
+                            serverDown = true;
+                        }
+                    } else {
+                        if (serverDown) {
+                            out.write(words[1].getBytes());
+                            out.write(System.lineSeparator().getBytes());
+                            serverDown = false;
+                        }
+                    }
+                    line = reader.readLine();
                 }
-            } else {
-                if (serverDown) {
-                    listOut.add(logLine.concat(entry.getKey()));
-                    logLine = "";
-                    serverDown = false;
-                }
-            }
-        }
-        try (FileOutputStream out = new FileOutputStream(target)) {
-            for (String line : listOut) {
-                out.write(line.getBytes());
-                out.write(System.lineSeparator().getBytes());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -59,3 +44,4 @@ public class Analysis {
         }
     }
 }
+
