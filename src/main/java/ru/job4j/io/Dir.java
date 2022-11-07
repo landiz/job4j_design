e9fccd8a6c3c2576310a30c0b1e2575261185984
landiz <landizm@yandex.ru>
2022-11-07 16:57:28 +0300

    4.0. File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\temp");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        for (File subfile : file.listFiles()) {
+            if (!subfile.isDirectory()) {
+                System.out.println(String.format(subfile.getName() + " : " + subfile.length() / 1024 + "Kb"));
+            }
+        }
+    }
+}
