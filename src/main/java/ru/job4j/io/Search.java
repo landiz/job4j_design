5a8719eda71149bcfa86ae0dee6db9e9bd79a63a
ayazev <yazev73@gmail.com>
2022-12-05 17:35:32 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,17 +1,30 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
     @SuppressWarnings("checkstyle:GenericWhitespace")
     public static void main(String[] args) throws IOException {
-        Path startingDir = Paths.get("C:\\temp");
-        search(startingDir, p -> p.toFile().getName().endsWith(".txt"));
+        checkArgs(args);
+        search(Path.of(args[0]), p -> p.toFile().getName().endsWith(args[1]));
+    }
+
+    private static void checkArgs(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Specify the starting folder and extension of the files to be searched.");
+        }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Folder not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
     }
 
     public static List<Path> search(Path startingDir, Predicate<Path> fileToSearch) throws IOException {
