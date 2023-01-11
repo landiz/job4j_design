27dffa195cd531da68c6cfcd7900652a230dddc0
ayazev <yazev73@gmail.com>
2023-01-11 22:11:42 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,14 +1,19 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -19,7 +24,6 @@ public class EchoServer {
                     String str = in.readLine();
                     if (str.contains("Exit")) {
                         out.write("Exit".getBytes());
-                        server.close();
                     } else if (str.contains("Hello")) {
                         out.write("Hello".getBytes());
                     } else {
@@ -28,6 +32,8 @@ public class EchoServer {
                     out.flush();
                 }
             }
+        } catch (Exception e) {
+            LOG.error("Exception was processed while establish connection", e);
         }
     }
 }
