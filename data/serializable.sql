5a3b64c2625ecf24591951d01610218073046a1b
landiz <yazev73@gmail.com>
2023-09-18 19:02:43 +0300

    4.3. Транзакции. Уровни изолированности транзакций. [#504812]

---
+++ data/serializable.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS lamps
+(
+    id
+    serial
+    PRIMARY
+    KEY,
+    color
+    text,
+    state
+    text
+);
+
+INSERT INTO lamps(color, state)
+VALUES ('red', 'on'),
+       ('blue', 'off');
